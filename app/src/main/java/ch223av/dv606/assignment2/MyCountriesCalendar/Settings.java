1a0a89ee82840ca7154ae35ad2ef3b7da522ccc7
Christofer Lind <lind.christofer@gmail.com>
2016-09-22 11:57:35 +0200

    Empty settings activity added.

---
+++ app/src/main/java/ch223av/dv606/assignment2/MyCountriesCalendar/Settings.java
@@ -0,0 +1,20 @@
+package ch223av.dv606.assignment2.MyCountriesCalendar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ch223av.dv606.assignment2.R;
+
+public class Settings extends AppCompatActivity{
+
+    public static final String TAG = AddVisit.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+
+}
