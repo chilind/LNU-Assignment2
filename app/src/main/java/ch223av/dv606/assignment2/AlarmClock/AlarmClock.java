fc481c427e4b22199cf1464376a3054f07bdcb1e
Christofer Lind <lind.christofer@gmail.com>
2016-09-22 16:39:55 +0200

    Init exercise 3.

---
+++ app/src/main/java/ch223av/dv606/assignment2/AlarmClock/AlarmClock.java
@@ -0,0 +1,15 @@
+package ch223av.dv606.assignment2.AlarmClock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ch223av.dv606.assignment2.R;
+
+public class AlarmClock extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_clock_main);
+
+    }
+}
