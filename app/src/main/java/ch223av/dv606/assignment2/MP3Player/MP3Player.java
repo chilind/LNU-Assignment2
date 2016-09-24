46f5ebfaeac34028d9a2b258a937706007508148
Christofer Lind <lind.christofer@gmail.com>
2016-09-24 15:07:37 +0200

    service implemented

--- app/src/main/java/ch223av/dv606/assignment2/MP3Player/MP3Player.java
+++ app/src/main/java/ch223av/dv606/assignment2/MP3Player/MP3Player.java
@@ -1,6 +1,8 @@
 package ch223av.dv606.assignment2.MP3Player;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.media.AudioManager;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,11 +26,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import ch223av.dv606.assignment2.R;
-
 /**
  * A simple MP3 player skeleton for 2DV606 Assignment 2.
  *
@@ -39,17 +40,12 @@ public class MP3Player extends AppCompatActivity {
 
     private static final String TAG = MP3Player.class.getSimpleName();
 
-    // This is an oversimplified approach which you should improve
-    // Currently, if you exit/re-enter activity, a new instance of player is created
-    // and you can't, e.g., stop the playback for the previous instance,
-    // and if you click a song, you will hear another audio stream started
-    public final MediaPlayer mediaPlayer = new MediaPlayer();
+    public static final MediaPlayer mediaPlayer = new MediaPlayer();
     public static final String PLAYLISTPROGRESS = "PLAYLISTPROGRESS";
     public static final String MyPREFERENCES = "MyPrefs";
 
     public SharedPreferences sharedpreferences;
-
-
+    NotificationCompat.Builder mBuilder;
 
     Button mPreviousButton;
     Button mNextButton;
@@ -76,9 +72,8 @@ public class MP3Player extends AppCompatActivity {
         mPauseButton = (Button) findViewById(R.id.pauseButton);
         mPlayButton = (Button) findViewById(R.id.playButton);
 
-
-
         sharedpreferences = getApplicationContext().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
 
         mPauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +81,10 @@ public class MP3Player extends AppCompatActivity {
                 if(mediaPlayer.isPlaying()){
                     mediaPlayer.pause();
                     songProgress = mediaPlayer.getCurrentPosition();
+                }else {
+                    mediaPlayer.seekTo(songProgress);
+                    mediaPlayer.start();
+                    songProgress = 0;
                 }
             }
         });
@@ -93,7 +92,10 @@ public class MP3Player extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!mediaPlayer.isPlaying()){
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    songProgress = mediaPlayer.getCurrentPosition();
+                } else {
                     mediaPlayer.seekTo(songProgress);
                     mediaPlayer.start();
                     songProgress = 0;
@@ -135,8 +137,10 @@ public class MP3Player extends AppCompatActivity {
             }
         });
 
-        Log.i("Running","externalSongList()");
-        externalSongList();
+        Log.i(TAG,"Starting service");
+        Intent svc = new Intent(this, PlayService.class);
+        this.startService(svc);
+
 
         playlistListView.setAdapter(new PlayListAdapter(this, songs));
         playlistListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -233,69 +237,29 @@ public class MP3Player extends AppCompatActivity {
 
         return songs;
     }
-    private ArrayList<Song> externalSongList() {
-        ArrayList<Song> songs = new ArrayList<Song>();
 
-        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
-        path.mkdirs();
-
-
-        Cursor c = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                new String[] { "distinct " + MediaStore.Audio.Media.ALBUM,
-                        MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.ALBUM_KEY,
-                        MediaStore.Audio.Media.ALBUM_ID},
-                null,
-                null,
-                MediaStore.Audio.Media.ARTIST);
-        Log.i(TAG,path.toString());
-        try {
-            Log.i(TAG, c.getString(0));
-        }
-        catch (Exception e){
-            Log.i(TAG,e.toString());
-        }
-        return songs;
-
-        /*
-        if(!isStorageAvailable()) // Check for media storage
-        {
-            Toast.makeText(this, R.string.nosd, Toast.LENGTH_SHORT).show();
-            return songs;
-        }
-
-        Cursor music = getContentResolver().query( // using content resolver to read music from media storage
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                new String[]{
-                        MediaStore.Audio.Media.ARTIST,
-                        MediaStore.Audio.Media.ALBUM,
-                        MediaStore.Audio.Media.DISPLAY_NAME,
-                        MediaStore.Audio.Media.DATA},
-                MediaStore.Audio.Media.IS_MUSIC + " > 0 ",
-                null, null
-        );
-
-        if (music.getCount() > 0)
-        {
-            music.moveToFirst();
-            Song prev = null;
-            do
-            {
-                Song song = new Song(music.getString(0), music.getString(1), music.getString(2), music.getString(3));
-
-                if (prev != null) // play the songs in a playlist, if possible
-                    prev.setNext(song);
-
-                prev = song;
-                songs.add(song);
-            }
-            while(music.moveToNext());
-
-            prev.setNext(songs.get(0)); // play in loop
-        }
-        music.close();
-
-        return songs;*/
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("OnResume",""+(mediaPlayer.isPlaying()));
+        /*
+        try{
+            //mediaPlayer.reset(); // reset the resource of player
+            //mediaPlayer.setDataSource(this, Uri.parse(song.getPath())); // set the song to play
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION); // select the audio stream
+            mediaPlayer.prepare();
+        }catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }*/
+    }
+
+
+
     /**
      * Uses mediaPlayer to play the selected song.
      * The sequence of media player operations is crucial for it to work.
---
+++ app/src/main/java/ch223av/dv606/assignment2/MP3Player/PlayService.java
@@ -0,0 +1,72 @@
+package ch223av.dv606.assignment2.MP3Player;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import ch223av.dv606.assignment2.R;
+
+public class PlayService extends Service {
+
+    int mStartMode;       // indicates how to behave if the service is killed
+    IBinder mBinder;      // interface for clients that bind
+    boolean mAllowRebind; // indicates whether onRebind should be used
+    NotificationCompat.Builder mBuilder;
+    public static final String TAG = "PlayService";
+    private boolean isRunning = false;
+
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "Service onCreate");
+        isRunning = true;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // The service is starting, due to a call to startService()
+        final String action = intent == null ? null : intent.getAction();
+        Log.i(TAG,"service action:" + action);
+
+
+        mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setTicker("ticker");
+
+
+        Intent notificationIntent = new Intent(this, MP3Player.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        mBuilder.setContentIntent(pi);
+        Notification notification = mBuilder.build();
+
+        //mBuilder.setAutoCancel(true);
+        startForeground(1, notification);
+
+        return mStartMode;
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // A client is binding to the service with bindService()
+        return mBinder;
+    }
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // All clients have unbound with unbindService()
+        return mAllowRebind;
+    }
+    @Override
+    public void onRebind(Intent intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
+    }
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+    }
+}
+
